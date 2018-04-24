d95885c1543858d48c418c773c932f3aade89905
Issam <issam.mokram@gmail.com>
2018-04-24 11:40:47 +0200

    V1.0
    Première iteration du jeu des lumières

---
+++ Assets/Scripts/Lights.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour {
+	public GameObject prefabLight;
+	// Use this for initialization
+	void Start () {
+		Instantiate (prefabLight);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
